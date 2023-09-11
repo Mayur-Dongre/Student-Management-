3b9bcaf6fdade5d9ad67b42dacc0d3b7f44dab31
Mayur-Dongre <mayurdongremd07@gmail.com>
2023-09-11 15:56:48 +0530

    Project Backend & Frontend

---
+++ backend/Models/StudentDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentManagement.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Student> Student { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB; Initial Catalog=school; TrustServerCertificate= True");
+        }
+    }
+}
---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import StudentCrud from './components/StudentCrud';
+
+function App() {
+  return (
+    <div className="App">
+      <StudentCrud/>
+    </div>
+  );
+}
+
+export default App;
